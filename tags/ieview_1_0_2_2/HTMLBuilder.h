da223a6058c06dffdf6d1985e6c1d06419a60897
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2005-04-11 21:56:33 +0000

    made a copy

---
+++ tags/ieview_1_0_2_2/HTMLBuilder.h
@@ -0,0 +1,80 @@
+/*
+
+IEView Plugin for Miranda IM
+Copyright (C) 2005  Piotr Piastucki
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+class HTMLBuilder;
+
+#ifndef HTMLBUILDER_INCLUDED
+#define HTMLBUILDER_INCLUDED
+
+#include "IEView.h"
+
+class TextToken {
+private:
+	int  type;
+	wchar_t *wtext;
+	char *text;
+	wchar_t *wlink;
+	char *link;
+	TextToken *next;
+public:
+	enum TOKENS {
+		END      = 0,
+		TEXT,
+		LINK,
+		WWWLINK,
+		SMILEY,
+	};
+	TextToken(int type, const char *text, int len);
+	TextToken(int type, const wchar_t *wtext, int len);
+	~TextToken();
+	int getType();
+	const char *		getText();
+	const wchar_t*      getTextW();
+	const char *		getLink();
+	const wchar_t *		getLinkW();
+	void 				setLink(const char *link);
+	void 				setLink(const wchar_t *wlink);
+	TextToken *			getNext();
+	void   				setNext(TextToken *);
+	void				toString(char **str, int *sizeAlloced);
+	void				toString(wchar_t **str, int *sizeAlloced);
+	static char *		urlEncode(const char *str);
+	static char *		urlEncode2(const char *str);
+	static TextToken* 	tokenizeLinks(const char *text);
+	static TextToken*	tokenizeSmileys(const char *proto, const char *text);
+	// UNICODE
+	wchar_t *			urlEncode(const wchar_t *str);
+	static TextToken* 	tokenizeLinks(const wchar_t *wtext);
+	static TextToken* 	tokenizeSmileys(const char *proto, const wchar_t *wtext);
+};
+
+class HTMLBuilder {
+protected:
+	virtual char *encode(const char *text, const char *proto, bool replaceSmiley);
+	virtual wchar_t *encode(const wchar_t *text, const char *proto, bool replaceSmiley);
+	virtual char *encodeUTF8(const wchar_t *text, const char *proto, bool replaceSmiley);
+	virtual char *encodeUTF8(const char *text, const char *proto, bool replaceSmiley);
+	virtual char *encodeUTF8(const char *text, int cp, const char *proto, bool replaceSmiley);
+public:
+	virtual void buildHead(IEView *, IEVIEWEVENT *event)=0;
+	virtual void appendEvent(IEView *, IEVIEWEVENT *event)=0;
+};
+
+#endif
---
+++ tags/ieview_1_0_2_2/Utils.cpp
@@ -0,0 +1,219 @@
+#include "Utils.h"
+
+static int countNoWhitespace(const wchar_t *str) {
+	int c;
+	for (c=0; *str!='\n' && *str!='\r' && *str!='\t' && *str!=' ' && *str!='\0'; str++, c++);
+	return c;
+}
+
+void Utils::appendText(char **str, int *sizeAlloced, const char *fmt, ...) {
+	va_list vararg;
+	char *p;
+	int size, len;
+
+	if (str == NULL) return;
+
+	if (*str==NULL || *sizeAlloced<=0) {
+		*sizeAlloced = size = 2048;
+		*str = (char *) malloc(size);
+		len = 0;
+	}
+	else {
+		len = strlen(*str);
+		size = *sizeAlloced - strlen(*str);
+	}
+
+	p = *str + len;
+	va_start(vararg, fmt);
+	while (_vsnprintf(p, size, fmt, vararg) == -1) {
+		size += 2048;
+		(*sizeAlloced) += 2048;
+		*str = (char *) realloc(*str, *sizeAlloced);
+		p = *str + len;
+	}
+	va_end(vararg);
+}
+
+void Utils::appendText(wchar_t **str, int *sizeAlloced, const wchar_t *fmt, ...) {
+	va_list vararg;
+	wchar_t *p;
+	int size, len;
+
+	if (str == NULL) return;
+
+	if (*str==NULL || *sizeAlloced<=0) {
+		*sizeAlloced = size = 2048;
+		*str = (wchar_t *) malloc(size);
+		len = 0;
+	}
+	else {
+		len = wcslen(*str);
+		size = *sizeAlloced - sizeof(wchar_t) * wcslen(*str);
+	}
+
+	p = *str + len;
+	va_start(vararg, fmt);
+	while (_vsnwprintf(p, size / sizeof(wchar_t), fmt, vararg) == -1) {
+		size += 2048;
+		(*sizeAlloced) += 2048;
+		*str = (wchar_t *) realloc(*str, *sizeAlloced);
+		p = *str + len;
+	}
+	va_end(vararg);
+}
+
+char *Utils::dupString(const char *a) {
+	if (a!=NULL) {
+		char *b = new char[strlen(a)+1];
+		strcpy(b, a);
+		return b;
+	}
+	return NULL;
+}
+
+char *Utils::dupString(const char *a, int l) {
+	if (a!=NULL) {
+		char *b = new char[l+1];
+		strncpy(b, a, l);
+		b[l] ='\0';
+		return b;
+	}
+	return NULL;
+}
+
+wchar_t *Utils::dupString(const wchar_t *a) {
+	if (a!=NULL) {
+		wchar_t *b = new wchar_t[wcslen(a)+1];
+		wcscpy(b, a);
+		return b;
+	}
+	return NULL;
+}
+
+wchar_t *Utils::dupString(const wchar_t *a, int l) {
+	if (a!=NULL) {
+		wchar_t *b = new wchar_t[l+1];
+		wcsncpy(b, a, l);
+		b[l] ='\0';
+		return b;
+	}
+	return NULL;
+}
+
+
+wchar_t *Utils::convertToWCS(const char *a) {
+	if (a!=NULL) {
+		int len = strlen(a)+1;
+		wchar_t *b = new wchar_t[len];
+		MultiByteToWideChar(CP_ACP, 0, a, len, b, len);
+		return b;
+	}
+	return NULL;
+}
+
+wchar_t *Utils::convertToWCS(const char *a, int cp) {
+	if (a!=NULL) {
+		int len = strlen(a)+1;
+		wchar_t *b = new wchar_t[len];
+		MultiByteToWideChar(cp, 0, a, len, b, len);
+		return b;
+	}
+	return NULL;
+}
+
+char *Utils::convertToString(const wchar_t *a) {
+	if (a!=NULL) {
+		int len = wcslen(a)+1;
+		char *b = new char[len];
+		WideCharToMultiByte(CP_ACP, 0, a, len, b, len, NULL, FALSE);
+		return b;
+	}
+	return NULL;
+}
+
+
+char *Utils::convertToString(const wchar_t *a, int cp) {
+	if (a!=NULL) {
+		int len = wcslen(a)+1;
+		char *b = new char[len];
+		WideCharToMultiByte(cp, 0, a, len, b, len, NULL, FALSE);
+		return b;
+	}
+	return NULL;
+}
+
+void Utils::convertPath(char *path) {
+   	for (; *path!='\0'; path++) {
+   	    if (*path == '\\') *path = '/';
+   	}
+}
+
+
+DWORD Utils::safe_wcslen(wchar_t *msg, DWORD maxLen) {
+    DWORD i;
+	for (i = 0; i < maxLen; i++) {
+		if (msg[i] == (wchar_t)0)
+			return i;
+	}
+	return 0;
+}
+
+char * Utils::UTF8Encode(const wchar_t *wtext) {
+	unsigned char *szOut;
+	int len, i;
+	const wchar_t *w;
+
+	if (wtext == NULL) return NULL;
+	for (len=0, w=wtext; *w; w++) {
+		if (*w < 0x0080) len++;
+		else if (*w < 0x0800) len += 2;
+		else len += 3;
+	}
+	szOut = new unsigned char [len+1];
+	if (szOut == NULL) return NULL;
+
+	for (i=0, w=wtext; *w; w++) {
+		if (*w < 0x0080)
+			szOut[i++] = (unsigned char) *w;
+		else if (*w < 0x0800) {
+			szOut[i++] = 0xc0 | ((*w) >> 6);
+			szOut[i++] = 0x80 | ((*w) & 0x3f);
+		}
+		else {
+			szOut[i++] = 0xe0 | ((*w) >> 12);
+			szOut[i++] = 0x80 | (((*w) >> 6) & 0x3f);
+			szOut[i++] = 0x80 | ((*w) & 0x3f);
+		}
+	}
+	szOut[i] = '\0';
+	return (char *) szOut;
+}
+
+char *Utils::UTF8Encode(const char *text) {
+    wchar_t *wtext = Utils::convertToWCS(text);
+    char *atext = UTF8Encode(wtext);
+	delete wtext;
+	return atext;
+}
+
+void Utils::UTF8Encode(const char *text, char *output, int maxLen) {
+    wchar_t *wtext = Utils::convertToWCS(text);
+    char *atext = UTF8Encode(wtext);
+    memcpy(output, atext, min ((int)strlen(atext)+1, maxLen));
+	delete atext;
+	delete wtext;
+}
+
+int Utils::detectURL(const wchar_t *text) {
+	int i, len;
+	for (i=len=0;text[i]!='\0';i++, len++) {
+		if (!((text[i] >= '0' && text[i]<='9') || (text[i] >= 'A' && text[i]<='Z') || (text[i] >= 'a' && text[i]<='z'))) {
+			break;
+		}
+	}
+	if (len > 0 && text[i]==':' && text[i+1]=='/') {
+		len += countNoWhitespace(text+i);
+		return len;
+	}
+	return 0;
+}
