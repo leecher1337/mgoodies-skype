49a8ed6bc1dec7052cdb9e394972b24febc43555
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2005-04-16 21:19:59 +0000

    * gcc compatibility fixes

--- trunk/scriver/commonheaders.h
+++ trunk/scriver/commonheaders.h
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #if defined(UNICODE)
    #define _UNICODE
 #endif
+#include <wchar.h>
 #include <tchar.h>
 #define _WIN32_WINNT 0x0501
 #define _WIN32_IE 0x0500
