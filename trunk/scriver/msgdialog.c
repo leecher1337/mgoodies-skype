546bdd3441aa356abc1097fac51aab286f12ba37
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2005-05-16 18:03:43 +0000

    * DM_GETWINDOWSTATE fixed

--- trunk/scriver/msgdialog.c
+++ trunk/scriver/msgdialog.c
@@ -1371,7 +1371,8 @@ BOOL CALLBACK DlgProcMessage(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lPara
 				state |= MSG_WINDOW_STATE_FOCUS;
 			if (IsIconic(hwndDlg))
 				state |= MSG_WINDOW_STATE_ICONIC;
-			return state;
+			SetWindowLong(hwndDlg, DWL_MSGRESULT, state);
+			return TRUE;
 
 		}
 	case WM_ACTIVATE:
