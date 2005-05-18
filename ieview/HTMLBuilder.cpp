449c51d6cf1caaf462a48e132ff753ab32673598
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2005-05-18 20:12:59 +0000

    + "unevenly" aligned bbcodes

--- ieview/HTMLBuilder.cpp
+++ ieview/HTMLBuilder.cpp
@@ -418,7 +418,151 @@ TextToken* TextToken::tokenizeMath(const wchar_t *text) {
 	return NULL;
 }
 
+TextToken* TextToken::tokenizeBBCodes(const wchar_t *text, int l) {
+	static wchar_t *bbTagName[] = {L"b", L"i", L"u", L"img", L"color", L"size"};
+	static int 		bbTagNameLen[] = {1, 1, 1, 3, 5, 4};
+	static int 		bbTagArg[] = {0, 0, 0, 0, 1, 1};
+	static int 		bbTagId[] = {BB_B, BB_I, BB_U, BB_IMG, BB_COLOR, BB_SIZE};
+	static int      bbTagEnd[6];
+	static int      bbTagCount[6];
+	int i,j;
+   	TextToken *firstToken = NULL, *lastToken = NULL, * bbToken = NULL;
+    int textLen = 0;
+	for (j = 0; j < 6; j++) {
+		bbTagCount[j] = 0;
+		bbTagEnd[j] = 0;
+	}
+    for (i = 0; i <= l;) {
+		int k, tagArgStart=0, tagArgEnd=0, tagDataStart=0, newTokenType = 0, newTokenSize = 0;
+		bool bbFound = false;
+		if (text[i] == '[') {
+			if (text[i+1] != '/') {
+				for (j = 0; j < 6; j++) {
+					k = i + 1;
+					if (!wcsnicmp(text+k, bbTagName[j], bbTagNameLen[j])) {
+						tagArgStart = tagArgEnd = 0;
+						k += bbTagNameLen[j];
+						if (bbTagArg[j]) {
+							if (text[k] != '=') continue;
+							k++;
+							tagArgStart = k;
+							for (; text[k]!='\0'; k++) {
+								if ((text[k]>='0' && text[k]<='9') ||
+									(text[k]>='A' && text[k]<='Z') ||
+									(text[k]>='a' && text[k]<='z') ||
+									(text[k]=='#')) continue;
+								break;
+							}
+							tagArgEnd = k;
+						}
+						if (text[k] == ']') {
+							k++;
+							tagDataStart = k;
+							if (k < bbTagEnd[j]) k = bbTagEnd[j];
+							for (; k < l; k++) {
+								if (text[k] == '[' && text[k+1] == '/') {
+									k += 2;
+									if (!wcsnicmp(text+k, bbTagName[j], bbTagNameLen[j])) {
+										k += bbTagNameLen[j];
+										if (text[k] == ']') {
+											k++;
+											bbFound = true;
+											break;
+										}
+									}
+								}
+							}
+							if (bbFound) break;
+						}
+					}
+				}
+				if (bbFound) {
+                    bbTagEnd[j] = k;
+					switch (bbTagId[j]) {
+					case BB_B:
+					case BB_I:
+					case BB_U:
+					case BB_COLOR:
+					case BB_SIZE:
+	                    bbTagCount[j]++;
+						if (bbTagArg[j]) {
+							bbToken = new TextToken(BBCODE, text + tagArgStart, tagArgEnd - tagArgStart);
+						} else {
+							bbToken = new TextToken(BBCODE, bbTagName[j], bbTagNameLen[j]);
+						}
+						bbToken->setEnd(false);
+						bbToken->setTag(bbTagId[j]);
+						newTokenType = BBCODE;
+						newTokenSize = tagDataStart - i;
+						break;
+					case BB_IMG:
+						bbToken = new TextToken(BBCODE, text + tagDataStart, k - tagDataStart);
+						bbToken->setTag(bbTagId[j]);
+						bbToken->setEnd(false);
+						newTokenType = BBCODE;
+						newTokenSize = k - i;
+						break;
+					}
+				}
+			} else {
+				for (j = 0; j < 6; j++) {
+					k = i + 2;
+					if (bbTagCount[j]>0 && !wcsnicmp(text+k, bbTagName[j], bbTagNameLen[j])) {
+						k += bbTagNameLen[j];
+						if (text[k] == ']') {
+							k++;
+							bbFound = true;
+							break;
+						}
+					}
+				}
+				if (bbFound) {
+                    bbTagCount[j]--;
+					bbToken = new TextToken(BBCODE, bbTagName[j], bbTagNameLen[j]);
+					bbToken->setEnd(true);
+					bbToken->setTag(bbTagId[j]);
+					newTokenType = BBCODE;
+					newTokenSize = k - i;
+				}
+			}
+		}
+		if (!bbFound) {
+			if (i==l) {
+				newTokenType = END;
+				newTokenSize = 1;
+			} else {
+				newTokenType = TEXT;
+				newTokenSize = 1;
+			}
+		}
+		if (newTokenType != TEXT) {
+			if (textLen >0 ) {
+                TextToken *newToken = new TextToken(TEXT, text+i-textLen, textLen);
+				textLen = 0;
+				if (lastToken == NULL) {
+					firstToken = newToken;
+				} else {
+				    lastToken->setNext(newToken);
+				}
+				lastToken = newToken;
+			}
+            if (newTokenType == BBCODE) {
+				if (lastToken == NULL) {
+					firstToken = bbToken;
+				} else {
+				    lastToken->setNext(bbToken);
+				}
+				lastToken = bbToken;
+            }
+		} else {
+			textLen += newTokenSize;
+		}
+		i += newTokenSize;
+    }
+    return firstToken;
+}
 
+/*
 TextToken* TextToken::tokenizeBBCodes(const wchar_t *text, int l) {
 	static wchar_t *bbTagName[] = {L"b", L"i", L"u", L"img", L"color", L"size"};
 	static int 		bbTagNameLen[] = {1, 1, 1, 3, 5, 4};
@@ -537,7 +681,7 @@ TextToken* TextToken::tokenizeBBCodes(const wchar_t *text, int l) {
     }
     return firstToken;
 }
-
+*/
 TextToken* TextToken::tokenizeLinks(const wchar_t *text) {
     TextToken *firstToken = NULL, *lastToken = NULL;
     int textLen = 0;
@@ -753,13 +897,16 @@ void TextToken::toString(wchar_t **str, int *sizeAlloced) {
 			if (!end) {
 				switch (tag) {
 				case BB_B:
-					Utils::appendText(str, sizeAlloced, L"<span style=\"font-weight: bold;\">");
+					//Utils::appendText(str, sizeAlloced, L"<span style=\"font-weight: bold;\">");
+					Utils::appendText(str, sizeAlloced, L"<b>");
 					break;
 				case BB_I:
-					Utils::appendText(str, sizeAlloced, L"<span style=\"font-style: italic;\">");
+					//Utils::appendText(str, sizeAlloced, L"<span style=\"font-style: italic;\">");
+					Utils::appendText(str, sizeAlloced, L"<i>");
 					break;
 				case BB_U:
-					Utils::appendText(str, sizeAlloced, L"<span style=\"text-decoration: underline;\">");
+					//Utils::appendText(str, sizeAlloced, L"<span style=\"text-decoration: underline;\">");
+					Utils::appendText(str, sizeAlloced, L"<u>");
 					break;
 				case BB_IMG:
             		eText = urlEncode(wtext);   // 100%% //< document.body.clientWidth  ? this.parentNode.width : document.body.clientWidth
@@ -767,7 +914,8 @@ void TextToken::toString(wchar_t **str, int *sizeAlloced) {
         	    	break;
 				case BB_COLOR:
             		eText = urlEncode(wtext);
-        	    	Utils::appendText(str, sizeAlloced, L"<span style=\"color: %s;\">", eText);
+        	    	//Utils::appendText(str, sizeAlloced, L"<span style=\"color: %s;\">", eText);
+        	    	Utils::appendText(str, sizeAlloced, L"<font color =\"%s\">", eText);
         	    	break;
 				case BB_SIZE:
             		eText = urlEncode(wtext);
@@ -777,9 +925,17 @@ void TextToken::toString(wchar_t **str, int *sizeAlloced) {
 			} else {
 				switch (tag) {
 				case BB_B:
+					Utils::appendText(str, sizeAlloced, L"</b>");
+					break;
 				case BB_I:
+					Utils::appendText(str, sizeAlloced, L"</i>");
+					break;
 				case BB_U:
+					Utils::appendText(str, sizeAlloced, L"</u>");
+					break;
 				case BB_COLOR:
+					Utils::appendText(str, sizeAlloced, L"</font>");
+					break;
 				case BB_SIZE:
 					Utils::appendText(str, sizeAlloced, L"</span>");
 					break;
