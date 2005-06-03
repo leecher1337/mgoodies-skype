a43bf9d29a2b2ea338a0d8b790cc9fba4575dedd
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2005-06-03 19:55:53 +0000

    * stay minimized and do not bring new tabs to front fixed

--- scriver/msgs.c
+++ scriver/msgs.c
@@ -86,8 +86,8 @@ static int MessageEventAdded(WPARAM wParam, LPARAM lParam)
 		newData.hContact = (HANDLE) wParam;
 		if (g_dat->hParent == NULL || !(g_dat->flags & SMF_USETABS)) {
 			g_dat->hParent = CreateDialogParam(g_hInst, MAKEINTRESOURCE(IDD_MSGWIN), NULL, DlgProcParentWindow, (LPARAM) & newData);
-			newData.minimized = DBGetContactSettingByte(NULL, SRMMMOD, SRMSGSET_STAYMINIMIZED, SRMSGDEFSET_STAYMINIMIZED);
 		}
+		newData.minimized = 1;
 		CreateDialogParam(g_hInst, MAKEINTRESOURCE(IDD_MSG), g_dat->hParent, DlgProcMessage, (LPARAM) & newData);
 		return 0;
 	}
@@ -249,7 +249,7 @@ static void RestoreUnreadMessageAlerts(void)
 					HWND hwndParent;
 					struct NewMessageWindowLParam newData = { 0 };
 					newData.hContact = hContact;
-					newData.minimized = DBGetContactSettingByte(NULL, SRMMMOD, SRMSGSET_STAYMINIMIZED, SRMSGDEFSET_STAYMINIMIZED);
+					newData.minimized = 1;
 					hwndParent = CreateDialogParam(g_hInst, MAKEINTRESOURCE(IDD_MSGWIN), NULL, DlgProcParentWindow, (LPARAM) & newData);
 					CreateDialogParam(g_hInst, MAKEINTRESOURCE(IDD_MSG), hwndParent, DlgProcMessage, (LPARAM) & newData);
 //					CreateDialogParam(g_hInst, MAKEINTRESOURCE(IDD_MSG), NULL, DlgProcMessage, (LPARAM) & newData);
