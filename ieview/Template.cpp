8f843bbf31ef89472573bb8a0818be45b66fe773
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2005-06-25 20:44:24 +0000

    + new variables: %statusMsg%,  %nickIn% * bugfix: memory leak (string returned by CNF_DISPLAY was not freed)

--- ieview/Template.cpp
+++ ieview/Template.cpp
@@ -92,6 +92,8 @@ static TokenDef tokenNames[] = {
 	{"%uin%", Token::UIN, 5, 0},
 	{"%uinIn%", Token::UININ, 7, 0},
 	{"%uinOut%", Token::UINOUT, 8, 0},
+	{"%statusMsg%", Token::STATUSMSG, 11, 0},
+	{"%nickIn%", Token::NICKIN, 8, 0},
 
 	{"%\\name%", Token::NAME, 7, 1},
 	{"%\\time%", Token::TIME, 7, 1},
@@ -107,7 +109,9 @@ static TokenDef tokenNames[] = {
 	{"%\\nameOut%", Token::NAMEOUT, 10, 1},
 	{"%\\uin%", Token::UIN, 6, 1},
 	{"%\\uinIn%", Token::UININ, 8, 1},
-	{"%\\uinOut%", Token::UINOUT, 9, 1}
+	{"%\\uinOut%", Token::UINOUT, 9, 1},
+	{"%\\statusMsg%", Token::STATUSMSG, 12, 1},
+	{"%\\nickIn%", Token::NICKIN, 9, 1}
 };
 
 void Template::tokenize() {
