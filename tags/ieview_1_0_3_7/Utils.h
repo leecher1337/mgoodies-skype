c004e2b01015efc04929cde9143aa7530071d652
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2005-08-22 19:01:08 +0000

    made a copy

---
+++ tags/ieview_1_0_3_7/Utils.h
@@ -0,0 +1,30 @@
+class Utils;
+
+#ifndef UTILS_INCLUDED
+#define UTILS_INCLUDED
+
+#include "ieview_common.h"
+
+class Utils {
+public:
+	static void appendText(char **str, int *sizeAlloced, const char *fmt, ...);
+	static void appendText(wchar_t **str, int *sizeAlloced, const wchar_t *fmt, ...);
+	static void convertPath(char *path);
+	static char *dupString(const char *a);
+	static char *dupString(const char *a, int l);
+	static wchar_t *dupString(const wchar_t *a);
+	static wchar_t *dupString(const wchar_t *a, int l);
+	static wchar_t *convertToWCS(const char *a);
+	static wchar_t *convertToWCS(const char *a, int cp);
+	static char *convertToString(const wchar_t *a);
+	static char *convertToString(const wchar_t *a, int cp);
+	static char *escapeString(const char *a);
+	static DWORD safe_wcslen(wchar_t *msg, DWORD maxLen);
+	static char *UTF8Encode(const wchar_t *wtext);
+	static char *UTF8Encode(const char *text);
+	static void  UTF8Encode(const char *text, char *output, int maxLen);
+	static int   detectURL(const wchar_t *text);
+};
+
+#endif
+
