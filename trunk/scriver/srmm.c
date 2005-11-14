086ab28588275ce667a8d1c398d2072f6688948d
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2005-11-14 23:26:38 +0000

    * log formatting improvements

--- trunk/scriver/srmm.c
+++ trunk/scriver/srmm.c
@@ -32,7 +32,7 @@ HINSTANCE g_hInst;
 PLUGININFO pluginInfo = {
 	sizeof(PLUGININFO),
 	"Scriver",
-	PLUGIN_MAKE_VERSION(2, 2, 9, 1),
+	PLUGIN_MAKE_VERSION(2, 2, 9, 2),
 #ifdef _UNICODE
 	"Scriver - send and receive instant messages (Unicode)",
 #else
