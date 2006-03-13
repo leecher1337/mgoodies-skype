623e1fa8e6c6fe0cc074ca713e9c527a22f973bb
tweety <tweety@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-03-13 11:49:01 +0000

    ! Bug fix on left click popup.

--- yamn/main.h
+++ yamn/main.h
@@ -2,7 +2,7 @@
 #define __MAIN_H
 
 //For updater
-//#define YAMN_9x
+#define YAMN_9x
 #ifdef YAMN_9x
 	#define YAMN_SHORTNAME "YAMN tweety win9x"
 	#define YAMN_FILENAME "yamn_9x"
@@ -11,7 +11,7 @@
 	#define YAMN_FILENAME "yamn"
 #endif
 
-#define	YAMN_VERSION			PLUGIN_MAKE_VERSION(0,0,1,0) 	//ok but do not forget rewrite version for debug release in debug.cpp and POP3 filter in pop3comm.cpp
+#define	YAMN_VERSION			PLUGIN_MAKE_VERSION(0,0,1,1) 	//ok but do not forget rewrite version for debug release in debug.cpp and POP3 filter in pop3comm.cpp
 #define YAMN_NEWMAILSNDDESC		"YAMN: new mail"
 #define YAMN_CONNECTFAILSNDDESC	"YAMN: connect failed"
 #define	YAMN_CONNECTFAILSOUND	"YAMN/Sound/ConnectFail"
