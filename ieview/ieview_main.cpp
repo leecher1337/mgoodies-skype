3ca11f865be5a4930e21d07b85c517484a5539f6
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-03-22 15:43:52 +0000

    + url bbcode

--- ieview/ieview_main.cpp
+++ ieview/ieview_main.cpp
@@ -37,8 +37,8 @@ static int PreShutdown(WPARAM wParam, LPARAM lParam);
 PLUGININFO pluginInfo = {
 	sizeof(PLUGININFO),
 	"IEView",
-	PLUGIN_MAKE_VERSION(1,0,9,0),
-	"IE Based Chat Log (1.0.9.0 "__DATE__")",
+	PLUGIN_MAKE_VERSION(1,0,9,1),
+	"IE Based Chat Log (1.0.9.1 "__DATE__")",
 	"Piotr Piastucki",
 	"the_leech@users.berlios.de",
 	"(c) 2005-2006 Piotr Piastucki",
