da141121de365fe371c257739e1776e286486209
y_b <y_b@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-05-09 13:53:00 +0000

    LastSeenMod: Unhide the option to enable clist online notify. Release 5.0.4.0

--- lastseen-mod/resource.h
+++ lastseen-mod/resource.h
@@ -72,18 +72,20 @@
 #define IDC_SENDMSG                     1045
 #define IDC_LABTTITLE                   1046
 #define IDC_OPTIONSTAB                  1047
+#define IDC_DISWATCHED                  1048
+#define IDC_DISNONWATCHED               1049
 #define IDC_MAINGROUP                   -1
 #define IDC_INFO                        -1
 #define IDC_TEXT                        -1
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        112
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1048
+#define _APS_NEXT_CONTROL_VALUE         1050
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
