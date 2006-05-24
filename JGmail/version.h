a92fd07ab02a9682bf29e23cd455faa924406011
y_b <y_b@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-05-24 17:28:15 +0000

    JGmail: added "Visit GMail" menu item for the fake contact (if enabled) Version bump 0.4.3.2k

--- JGmail/version.h
+++ JGmail/version.h
@@ -1,3 +1,3 @@
 #define __FILEVERSION_STRING        0,4,3,2
-#define __VERSION_STRING            "0.4.3.2j"
+#define __VERSION_STRING            "0.4.3.2k"
 #define __VERSION_DWORD             0x00040302
