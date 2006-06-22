ca57aaf87d91954775177c4b7687be6dc59e9544
y_b <y_b@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-06-22 19:34:03 +0000

    JGmail URLencode the password before redirecting the browser version bump to 0.6.0.1n

--- trunk/JGmail/version.h
+++ trunk/JGmail/version.h
@@ -1,3 +1,3 @@
 #define __FILEVERSION_STRING        0,6,0,1
-#define __VERSION_STRING            "0.6.0.1m"
+#define __VERSION_STRING            "0.6.0.1n"
 #define __VERSION_DWORD             0x00060001
