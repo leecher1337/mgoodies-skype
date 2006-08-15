c2606efa47231c5501c6f2a78eb50766a16ef3b0
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-08-15 20:38:33 +0000

    made a copy

---
+++ tags/ieview_1_0_9_5/SRMMHTMLBuilder.h
@@ -0,0 +1,20 @@
+class SRMMHTMLBuilder;
+
+#ifndef SRMMHTMLBUILDER_INCLUDED
+#define SRMMHTMLBUILDER_INCLUDED
+
+#include "HTMLBuilder.h"
+
+class SRMMHTMLBuilder:public TemplateHTMLBuilder
+{
+protected:
+    virtual void loadMsgDlgFont(int i, LOGFONTA * lf, COLORREF * colour);
+	char *timestampToString(DWORD dwFlags, time_t check);
+	bool isDbEventShown(DBEVENTINFO * dbei);
+	void 	appendEventNonTemplate(IEView *, IEVIEWEVENT *event);
+public:
+	void buildHead(IEView *, IEVIEWEVENT *event);
+	void appendEvent(IEView *, IEVIEWEVENT *event);
+};
+
+#endif
---
+++ tags/ieview_1_0_9_5/TemplateHTMLBuilder.h
@@ -0,0 +1,29 @@
+class TemplateHTMLBuilder;
+
+#ifndef TEMPLATEHTMLBUILDER_INCLUDED
+#define TEMPLATEHTMLBUILDER_INCLUDED
+
+#include "HTMLBuilder.h"
+#include "Template.h"
+
+class TemplateHTMLBuilder:public HTMLBuilder
+{
+protected:
+		char *timestampToString(DWORD dwFlags, time_t check, int mode);
+		time_t 		startedTime;
+		time_t 		getStartedTime();
+		const char *groupTemplate;
+		time_t 		flashAvatarsTime[2];
+		char *		flashAvatars[2];
+		const char *getFlashAvatar(const char *file, int index);
+		void buildHeadTemplate(IEView *, IEVIEWEVENT *event, ProtocolSettings* protoSettings);
+		void appendEventTemplate(IEView *, IEVIEWEVENT *event, ProtocolSettings* protoSettings);
+		virtual TemplateMap *getTemplateMap(ProtocolSettings *);
+		virtual int getFlags(ProtocolSettings *);
+public:
+		TemplateHTMLBuilder();
+		virtual ~TemplateHTMLBuilder();
+//	void buildHead(IEView *, IEVIEWEVENT *event);
+};
+
+#endif
