ae90569f526d17e94afeb57e70b14ce9ff176191
y_b <y_b@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-09-14 22:18:09 +0000

    JGmail Version 0.6.0.18 Corrected behavior of option dialog in staticssl build

--- JGmail/version.h
+++ JGmail/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,6,0,17
-#define __VERSION_STRING            "0.6.0.17"
-#define __VERSION_DWORD             0x00060011
+#define __FILEVERSION_STRING        0,6,0,18
+#define __VERSION_STRING            "0.6.0.18"
+#define __VERSION_DWORD             0x00060012
