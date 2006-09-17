9ee46e7360288fa7a5931042c357c8eb730336e8
y_b <y_b@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-09-17 00:25:42 +0000

    JGmail small MIM 0.5.x compatibility fix ver 0.6.0.19

--- trunk/JGmail/jabber_opt.cpp
+++ trunk/JGmail/jabber_opt.cpp
@@ -757,7 +757,7 @@ int JabberOptInit( WPARAM wParam, LPARAM lParam )
 			pfnEnableThemeDialogTexture = (BOOL (WINAPI *)(HANDLE, DWORD))GetProcAddress(hUxTheme, "EnableThemeDialogTexture");
 	}
 
-	odp.cbSize = 0x3C;//sizeof( odp )-4; // this is not good!!!
+	odp.cbSize = sizeof( odp ); // should work correctly with defined MIRANDA_VER 0x0500
 	odp.hInstance = hInst;
 	odp.pszGroup = "Network";
 	odp.pszTemplate = MAKEINTRESOURCEA( IDD_OPT_EXPERT );
