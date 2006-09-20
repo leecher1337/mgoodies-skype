baa81556aa49e5d3330494181ebbb9ac007a06a4
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-09-20 18:17:48 +0000

    made a copy

---
+++ tags/ieview_1_0_9_6/Template.h
@@ -0,0 +1,101 @@
+class TemplateMap;
+class Template;
+#ifndef TEMPLATE_INCLUDED
+#define TEMPLATE_INCLUDED
+
+#include "ieview_common.h"
+
+class TokenDef {
+public:
+	const char *tokenString;
+	int 		token;
+	int         tokenLen;
+	int         escape;
+	TokenDef(const char *tokenString);
+	TokenDef(const char *tokenString, int token, int escape);
+};
+
+class Token {
+private:
+	int  escape;
+	int  type;
+	char *text;
+	Token *next;
+public:
+	enum TOKENS {
+		END      = 0,
+		BASE,
+		PLAIN,
+		TEXT,
+		NAME,
+		TIME,
+		DATE,
+		AVATAR,
+		CID,
+		PROTO,
+		AVATARIN,
+		AVATAROUT,
+		NAMEIN,
+		NAMEOUT,
+		UIN,
+		UININ,
+		UINOUT,
+		STATUSMSG,
+		NICKIN,
+		NICKOUT,
+		FILEDESC,
+	};
+	Token(int, const char *, int );
+	~Token();
+	int getType();
+	int getEscape();
+	const char *getText();
+	Token *getNext();
+	void   setNext(Token *);
+};
+
+class Template {
+private:
+	char *name;
+	char *text;
+	Template *next;
+	Token *tokens;
+protected:
+	friend class TemplateMap;
+	bool        equals(const char *name);
+	void        tokenize();
+	Template *	getNext();
+	Template(const char *name, const char *text);
+public:
+	~Template();
+	const char *getText();
+	const char *getName();
+	Token *getTokens();
+};
+
+class TemplateMap {
+private:
+	static TemplateMap *mapList;
+	char *				name;
+	char *				filename;
+	bool    			grouping;
+	Template *			entries;
+	TemplateMap *       next;
+	TemplateMap(const char *name);
+	void				addTemplate(const char *name, const char *text);
+	void				setFilename(const char *filename);
+	void                clear();
+	static TemplateMap*	add(const char *proto, const char *filename);
+	static void 		appendText(char **str, int *sizeAlloced, const char *fmt, ...);
+	static TemplateMap*	loadTemplateFile(const char *proto, const char *filename, bool onlyInfo);
+public:
+	static Template *	getTemplate(const char *proto, const char *name);
+	static TemplateMap *getTemplateMap(const char *proto);
+	static TemplateMap* loadTemplates(const char *proto, const char *filename);
+	Template *          getTemplate(const char *text);
+	const char *		getFilename();
+	bool        		isGrouping();
+};
+
+
+#endif
