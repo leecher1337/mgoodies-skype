6e99b6f8ce538a404cbf9c9c1706d93165c1c359
pescuma <pescuma@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-10-18 05:12:04 +0000

    mydetails: 0.0.1.3   + Support for ersatz plugin by TioDuke. When it is finished, status message will be shown correctly always :)

--- trunk/mydetails/mydetails.cpp
+++ trunk/mydetails/mydetails.cpp
@@ -31,7 +31,7 @@ PLUGINLINK *pluginLink;
 PLUGININFO pluginInfo={
 	sizeof(PLUGININFO),
 	"My Details",
-	PLUGIN_MAKE_VERSION(0,0,1,2),
+	PLUGIN_MAKE_VERSION(0,0,1,3),
 	"Show and allows you to edit your details for all protocols.",
 	"Ricardo Pescuma Domenecci",
 	"",
