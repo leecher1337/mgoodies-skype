bdd9822d309441ba1c66a21dc1d6aa3d89341858
pescuma <pescuma@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-10-29 18:35:30 +0000

    smh: . 0.1.0.2   * -pv- patch

--- trunk/smh/smh.cpp
+++ trunk/smh/smh.cpp
@@ -31,7 +31,7 @@ PLUGININFO pluginInfo = {
 #else
 	"Status Message History",
 #endif
-	PLUGIN_MAKE_VERSION(0,1,0,1),
+	PLUGIN_MAKE_VERSION(0,1,0,2),
 	"Log status message changes to history",
 	"Ricardo Pescuma Domenecci",
 	"",
