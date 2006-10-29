5731adee06c7843ef8c760f4b7298d4e02f18625
pescuma <pescuma@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-10-29 22:06:47 +0000

    spellchecker: start of it

---
+++ spellchecker/options.h
@@ -0,0 +1,89 @@
+/* 
+Copyright (C) 2006 Ricardo Pescuma Domenecci
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this file; see the file license.txt.  If
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  
+*/
+
+
+#ifndef __OPTIONS_H__
+# define __OPTIONS_H__
+
+
+#include "commons.h"
+
+#include <windows.h>
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+#define POPUP_ACTION_DONOTHING 0
+#define POPUP_ACTION_CLOSEPOPUP 1
+#define POPUP_ACTION_OPENHISTORY 2
+
+#define POPUP_DELAY_DEFAULT 0
+#define POPUP_DELAY_CUSTOM 1
+#define POPUP_DELAY_PERMANENT 2
+
+
+struct Options {
+	// Templates
+	TCHAR template_changed[1024];
+	TCHAR template_removed[1024];
+
+	// Track
+	BYTE track_changes;
+	BYTE track_removes;
+	BYTE track_only_not_offline;
+
+	// History
+	BYTE history_enable;
+	BYTE history_only_ansi_if_possible;
+
+	// Popup
+	BYTE popup_enable;
+	WORD popup_delay_type;
+	WORD popup_timeout;
+	BYTE popup_use_win_colors;
+	BYTE popup_use_default_colors;
+	COLORREF popup_bkg_color;
+	COLORREF popup_text_color;
+	WORD popup_left_click_action;
+	WORD popup_right_click_action;
+};
+
+extern Options opts;
+
+
+// Initializations needed by options
+void InitOptions();
+
+// Deinitializations needed by options
+void DeInitOptions();
+
+
+// Loads the options from DB
+// It don't need to be called, except in some rare cases
+void LoadOptions();
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __OPTIONS_H__
---
+++ spellchecker/resource.h
@@ -0,0 +1,57 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by resource.rc
+//
+#define IDD_OPTIONS                     119
+#define IDD_POPUPS                      120
+#define IDR_CONTEXT                     120
+#define IDC_DELAY                       1001
+#define IDC_WINCOLORS                   1002
+#define IDC_DEFAULTCOLORS               1003
+#define IDC_BGCOLOR                     1004
+#define IDC_TEXTCOLOR                   1005
+#define IDC_PREV                        1006
+#define IDC_DELAYFROMPU                 1007
+#define IDC_DELAYCUSTOM                 1008
+#define IDC_DELAYPERMANENT              1009
+#define IDC_RIGHT_ACTION                1022
+#define IDC_LEFT_ACTION                 1023
+#define IDC_PROTOCOLS                   1041
+#define IDC_CHANGED                     1058
+#define IDC_REMOVED                     1059
+#define IDC_CHECK1                      1060
+#define IDC_POPUPS                      1060
+#define IDC_CHECK2                      1061
+#define IDC_DELAY_SPIN                  1061
+#define IDC_HISTORY                     1061
+#define IDC_ANSI                        1062
+#define IDC_TRACK_G                     1063
+#define IDC_CHANGED_L                   1064
+#define IDC_REMOVED_L                   1065
+#define IDC_PROTOCOLS_G                 1066
+#define IDC_PROTOCOLS_L                 1067
+#define IDC_COLOURS_G                   1068
+#define IDC_TRACK_CHANGE                1068
+#define IDC_BGCOLOR_L                   1069
+#define IDC_TRACK_REMOVE                1069
+#define IDC_TEXTCOLOR_L                 1070
+#define IDC_ONLY_NOT_OFFLINE            1070
+#define IDC_DELAY_G                     1071
+#define IDC_ACTIONS_G                   1072
+#define IDC_RIGHT_ACTION_L              1073
+#define IDC_LEFT_ACTION_L               1074
+#define ID_POPUP_DISABLESPELLCHECKINGHERE 40004
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        121
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1075
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
