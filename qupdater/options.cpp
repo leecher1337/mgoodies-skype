f1129361409c37d1cdb0663e76d698e497ecc826
pescuma <pescuma@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2006-12-30 23:56:28 +0000

    qupdater: Helper service to retrieve plugin info using updater API

---
+++ qupdater/options.cpp
@@ -0,0 +1,170 @@
+/* 
+Copyright (C) 2006 Ricardo Pescuma Domenecci
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this file; see the file license.txt.  If
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  
+*/
+
+
+#include "commons.h"
+
+
+
+// Prototypes /////////////////////////////////////////////////////////////////////////////////////
+
+HANDLE hOptHook = NULL;
+
+Options opts;
+
+
+static BOOL CALLBACK OptionsDlgProc(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lParam);
+
+
+static OptPageControl optionsControls[] = { 
+	{ &opts.csv_file,			CONTROL_TEXT,		IDC_CSV,			"CSVFile",	(DWORD) _T("plugins.csv") },
+	{ &opts.dump_on_startup,	CONTROL_CHECKBOX,	IDC_DUMP_STARTUP,	"DumpOnStart",	FALSE }
+};
+
+
+
+// Functions //////////////////////////////////////////////////////////////////////////////////////
+
+
+int InitOptionsCallback(WPARAM wParam,LPARAM lParam)
+{
+	OPTIONSDIALOGPAGE odp;
+
+	ZeroMemory(&odp,sizeof(odp));
+    odp.cbSize=sizeof(odp);
+    odp.position=0;
+	odp.hInstance=hInst;
+	odp.ptszGroup = TranslateT("Services");
+	odp.ptszTitle = TranslateT("Q Updater");
+	odp.pfnDlgProc = OptionsDlgProc;
+	odp.pszTemplate = MAKEINTRESOURCEA(IDD_OPTIONS);
+    odp.flags = ODPF_BOLDGROUPS | ODPF_TCHAR | ODPF_EXPERTONLY;
+    CallService(MS_OPT_ADDPAGE,wParam,(LPARAM)&odp);
+
+	return 0;
+}
+
+
+void InitOptions()
+{
+	LoadOptions();
+	
+	hOptHook = HookEvent(ME_OPT_INITIALISE, InitOptionsCallback);
+}
+
+
+void DeInitOptions()
+{
+	UnhookEvent(hOptHook);
+}
+
+
+void LoadOptions()
+{
+	LoadOpts(optionsControls, MAX_REGS(optionsControls), MODULE_NAME);
+}
+
+
+void ListView_SetItemTextA( HWND hwndLV, int i, int iSubItem, char* pszText )
+{
+	LV_ITEMA _ms_lvi;
+	_ms_lvi.iSubItem = iSubItem;
+	_ms_lvi.pszText = pszText;
+	SendMessageA( hwndLV, LVM_SETITEMTEXTA, i, (LPARAM)&_ms_lvi);
+}
+
+
+static BOOL CALLBACK OptionsDlgProc(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lParam) 
+{
+	switch (msg) 
+	{
+		case WM_INITDIALOG:
+		{
+			TranslateDialogDefault(hwndDlg);
+
+			HWND hwndList = GetDlgItem(hwndDlg,IDC_PLUGINS);
+			
+			LVCOLUMN col = {0};
+			col.mask = LVCF_TEXT | LVCF_WIDTH;
+			col.pszText=TranslateT("Plugin Name");
+			col.cx=100;
+			ListView_InsertColumn(hwndList,0,&col);
+			
+			col.pszText=TranslateT("Version");			
+			col.cx=60;
+			ListView_InsertColumn(hwndList,1,&col);
+		
+			col.pszText=TranslateT("Update URL");
+			col.cx=200;
+			ListView_InsertColumn(hwndList,2,&col);
+		
+			col.pszText=TranslateT("Beta Download URL");
+			ListView_InsertColumn(hwndList,3,&col);
+		
+			col.pszText=TranslateT("Beta Version URL");
+			ListView_InsertColumn(hwndList,4,&col);
+		
+			col.pszText=TranslateT("Beta Changelog URL");
+			ListView_InsertColumn(hwndList,5,&col);
+			
+			// XXX: Won't work on windows 95 without IE3+ or 4.70
+			ListView_SetExtendedListViewStyleEx(hwndList, 0, LVS_EX_FULLROWSELECT );
+
+			for(int i = 0; i < plugins.size(); i++)
+			{
+				LVITEMA it = {0};
+				it.mask = LVIF_TEXT;
+				it.pszText = plugins[i].szComponentName;
+				int iRow = SendMessageA( hwndList, LVM_INSERTITEMA, 0, (LPARAM)&it );
+
+				if (plugins[i].pbVersion != NULL)
+					ListView_SetItemTextA(hwndList, iRow, 1, (char *) plugins[i].pbVersion);
+				if (plugins[i].szUpdateURL != NULL)
+				{
+					if (strcmp(plugins[i].szUpdateURL, UPDATER_AUTOREGISTER) == 0)
+						ListView_SetItemTextA(hwndList, iRow, 2, "<From FL XML>");
+					else
+						ListView_SetItemTextA(hwndList, iRow, 2, plugins[i].szUpdateURL);
+				}
+				if (plugins[i].szBetaUpdateURL != NULL)
+					ListView_SetItemTextA(hwndList, iRow, 3, plugins[i].szBetaUpdateURL);
+				if (plugins[i].szBetaVersionURL != NULL)
+					ListView_SetItemTextA(hwndList, iRow, 4, plugins[i].szBetaVersionURL);
+				if (plugins[i].szBetaChangelogURL != NULL)
+					ListView_SetItemTextA(hwndList, iRow, 5, plugins[i].szBetaChangelogURL);
+			}
+
+			// sort out the headers
+			ListView_SetColumnWidth(hwndList, 0, LVSCW_AUTOSIZE);
+
+			break;
+		}
+
+		case WM_COMMAND:
+		{
+			if (LOWORD(wParam) == IDC_DUMP)
+				DumpFile();
+			
+			break;
+		}
+	}
+
+	return SaveOptsDlgProc(optionsControls, MAX_REGS(optionsControls), MODULE_NAME, hwndDlg, msg, wParam, lParam);
+}
+
---
+++ qupdater/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by resource.rc
+//
+#define IDD_OPTIONS                     102
+#define IDC_EDIT1                       1000
+#define IDC_CSV                         1000
+#define IDC_DUMP                        1001
+#define IDC_PLUGINS                     1002
+#define IDC_DUMP_STARTUP                1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
