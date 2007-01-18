1ab3b22c77e44e9c339885c2f1808553eddda1ff
y_b <y_b@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2007-01-18 21:24:50 +0000

    YAMN: ! Some bad e-mail clients put quotes on charset name. Discard the quotes.

--- trunk/yamn/mails/decode.cpp
+++ trunk/yamn/mails/decode.cpp
@@ -213,6 +213,8 @@ int GetCharsetFromString(char *input,int size)
 	{
 		if ((*pin>='a') && (*pin<='z'))
 			*parser++=*(pin++)-('a'-'A'); // make it capital
+		else if(*pin=='\"')
+			*pin++; //skip the quotes if any
 		else 
 			*parser++=*pin++;
 	}
