89a853808eb9eaf80cbe7f8d00748c3d714a340c
y_b <y_b@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2007-01-19 14:43:42 +0000

    JGmail release 0.6.0.26

--- trunk/JGmail/version.h
+++ trunk/JGmail/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,6,0,25
-#define __VERSION_STRING            "0.6.0.25"
-#define __VERSION_DWORD             0x00060019
+#define __FILEVERSION_STRING        0,6,0,26
+#define __VERSION_STRING            "0.6.0.26"
+#define __VERSION_DWORD             0x0006001A
