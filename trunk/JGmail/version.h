f4cf7f276f1616b0abc50589a3931b415d9bbf6a
y_b <y_b@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2007-02-19 21:57:07 +0000

    JGmail: Big svn-sync on the way. r4860 (to make JGmail compilable with updated headers)

--- trunk/JGmail/version.h
+++ trunk/JGmail/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,6,0,26
-#define __VERSION_STRING            "0.6.0.26"
-#define __VERSION_DWORD             0x0006001A
+#define __FILEVERSION_STRING        0,6,0,27
+#define __VERSION_STRING            "0.6.0.27"
+#define __VERSION_DWORD             0x0006001B
