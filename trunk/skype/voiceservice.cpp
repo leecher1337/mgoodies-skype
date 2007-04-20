5c0114aaeb94c540f40e4af1fc89142b0de1c079
pescuma <pescuma@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2007-04-20 05:03:40 +0000

    skype: forgot to add new files

---
+++ trunk/skype/voiceservice.cpp
@@ -0,0 +1,150 @@
+#include "skype.h"
+#include "skypeapi.h"
+#include "voiceservice.h"
+#include "sdk/m_voiceservice.h"
+#include "../../include/m_utils.h"
+
+
+extern char pszSkypeProtoName[MAX_PATH+30];
+
+
+HANDLE hVoiceNotify = NULL;
+BOOL has_voice_service = FALSE;
+
+
+BOOL HasVoiceService()
+{
+	return has_voice_service;
+}
+
+void NofifyVoiceService(HANDLE hContact, char *callId, int state) 
+{
+	VOICE_CALL vc = {0};
+	vc.cbSize = sizeof(vc);
+	vc.szModule = pszSkypeProtoName;
+	vc.id = callId;
+	vc.flags = VOICE_CALL_CONTACT;
+	vc.state = state;
+	vc.hContact = hContact;
+	NotifyEventHooks(hVoiceNotify, (WPARAM) &vc, 0);
+}
+
+static int VoiceGetInfo(WPARAM wParam, LPARAM lParam)
+{
+	return VOICE_SUPPORTED | VOICE_CALL_CONTACT | VOICE_CAN_HOLD;
+}
+
+static HANDLE FindContactByCallId(char *callId)
+{
+	HANDLE hContact;
+	for (hContact = (HANDLE) CallService(MS_DB_CONTACT_FINDFIRST, 0, 0);
+			hContact != NULL;
+			hContact = (HANDLE) CallService(MS_DB_CONTACT_FINDNEXT, (WPARAM)hContact, 0)) 
+	{
+		char *szProto = (char*) CallService(MS_PROTO_GETCONTACTBASEPROTO, (WPARAM)hContact, 0);
+
+		DBVARIANT dbv;
+		if (szProto != NULL 
+			&& !strcmp(szProto, pszSkypeProtoName) 
+			&& DBGetContactSettingByte(hContact, pszSkypeProtoName, "ChatRoom", 0) == 0 
+			&& !DBGetContactSetting(hContact, pszSkypeProtoName, "CallId", &dbv)) 
+		{
+			if (!strcmp(callId, dbv.pszVal))
+				return hContact;
+		}
+	}
+
+	return NULL;
+}
+
+static int VoiceCall(WPARAM wParam, LPARAM lParam)
+{
+	if (wParam == NULL)
+		return -1;
+
+	DBVARIANT dbv;
+	if (DBGetContactSetting((HANDLE)wParam, pszSkypeProtoName, SKYPE_NAME, &dbv)) 
+		return -1;
+
+	char msg[512];
+	mir_snprintf(msg, sizeof(msg), "CALL %s", dbv.pszVal);
+	SkypeSend(msg);
+
+	return 0;
+}
+
+static int VoiceAnswer(WPARAM wParam, LPARAM lParam)
+{
+	if (wParam == NULL)
+		return -1;
+
+	char *callId = (char *) wParam;
+	if (FindContactByCallId(callId) == NULL)
+		return -1;
+
+	char msg[512];
+	mir_snprintf(msg, sizeof(msg), "SET %s STATUS INPROGRESS", callId);
+	SkypeSend(msg);
+	testfor("ERROR", 200);
+
+	return 0;
+}
+
+static int VoiceDrop(WPARAM wParam, LPARAM lParam)
+{
+	if (wParam == NULL)
+		return -1;
+
+	char *callId = (char *) wParam;
+	if (FindContactByCallId(callId) == NULL)
+		return -1;
+
+	char msg[512];
+	mir_snprintf(msg, sizeof(msg), "SET %s STATUS FINISHED", callId);
+	SkypeSend(msg);
+
+	return 0;
+}
+
+static int VoiceHold(WPARAM wParam, LPARAM lParam)
+{
+	if (wParam == NULL)
+		return -1;
+
+	char *callId = (char *) wParam;
+	if (FindContactByCallId(callId) == NULL)
+		return -1;
+
+	char msg[512];
+	mir_snprintf(msg, sizeof(msg), "SET %s STATUS ONHOLD", callId);
+	SkypeSend(msg);
+
+	return 0;
+}
+
+void VoiceServiceInit() 
+{
+	char szTmp[ 200 ];
+	mir_snprintf( szTmp, sizeof(szTmp), "%s" PE_VOICE_CALL_STATE, pszSkypeProtoName );
+	hVoiceNotify = CreateHookableEvent( szTmp );
+
+	mir_snprintf( szTmp, sizeof(szTmp), "%s" PS_VOICE_GETINFO, pszSkypeProtoName );
+	CreateServiceFunction( szTmp, VoiceGetInfo );
+
+	mir_snprintf( szTmp, sizeof(szTmp), "%s" PS_VOICE_CALL, pszSkypeProtoName );
+	CreateServiceFunction( szTmp, VoiceCall );
+
+	mir_snprintf( szTmp, sizeof(szTmp), "%s" PS_VOICE_ANSWERCALL, pszSkypeProtoName );
+	CreateServiceFunction( szTmp, VoiceAnswer );
+
+	mir_snprintf( szTmp, sizeof(szTmp), "%s" PS_VOICE_DROPCALL, pszSkypeProtoName );
+	CreateServiceFunction( szTmp, VoiceDrop );
+
+	mir_snprintf( szTmp, sizeof(szTmp), "%s" PS_VOICE_HOLDCALL, pszSkypeProtoName );
+	CreateServiceFunction( szTmp, VoiceHold );
+}
+
+void VoiceServiceModulesLoaded() 
+{
+	has_voice_service = ServiceExists(MS_VOICESERVICE_REGISTER);
+}
