7f6d2b79ad5b6126784538e62777f798cc5b46c6
pescuma <pescuma@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2007-04-20 05:03:40 +0000

    skype: forgot to add new files

---
+++ skype/voiceservice.h
@@ -0,0 +1,15 @@
+#ifndef _VOICESERVICE_H_
+#define _VOICESERVICE_H_
+
+#include "sdk/m_voice.h"
+
+
+BOOL HasVoiceService();
+void VoiceServiceInit();
+void VoiceServiceModulesLoaded();
+void NofifyVoiceService(HANDLE hContact, char *callId, int state) ;
+
+
+
+#endif // _VOICESERVICE_H_
+
