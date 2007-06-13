cc0659406fd06e452c41709187042e7b7165dfbe
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2007-06-13 17:41:18 +0000

    made a copy

---
+++ tags/ieview_1_0_9_10/ieview_common.h
@@ -0,0 +1,69 @@
+/*
+
+IEView Plugin for Miranda IM
+Copyright (C) 2005  Piotr Piastucki
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#if defined( UNICODE ) && !defined( _UNICODE )
+   #define _UNICODE
+#endif
+
+#ifndef IEVIEW_COMMON_H
+#define IEVIEW_COMMON_H
+
+#define MIRANDA_VER 0x0600
+
+#include <windows.h>
+#include <commctrl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stddef.h>
+#include <process.h>
+#include <stdarg.h>
+
+#include <newpluginapi.h>
+#include <m_utils.h>
+#include <m_clist.h>
+#include <m_langpack.h>
+#include <m_system.h>
+//#include <m_system.h>
+#include <m_protomod.h>
+#include <m_protosvc.h>
+//#include <m_clui.h>
+#include <m_options.h>
+//#include <m_userinfo.h>
+#include <m_database.h>
+#include "m_skin.h"
+#include <m_message.h>
+#include <m_contacts.h>
+#include <win2k.h>
+#include <richedit.h>
+#include "m_smileyadd.h"
+
+#include "m_ieview.h"
+
+#include "IEView.h"
+
+extern HINSTANCE hInstance;
+extern IEView *debugView;
+extern TCHAR *workingDir;
+extern char *ieviewModuleName;
+extern HANDLE hHookOptionsChanged;
+
+#endif
