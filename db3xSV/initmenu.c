981ef3ca39e969261603bd4d28ca7b49b7e6615b
y_b <y_b@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2007-08-12 15:48:39 +0000

    VirtDB: Hook ME_SYSTEM_PRESHUTDOWN should be better than ME_SYSTEM_OKTOEXIT

--- db3xSV/initmenu.c
+++ db3xSV/initmenu.c
@@ -80,7 +80,7 @@ static int extraInitMenus(WPARAM wParam, LPARAM lParam)
 {
 	hOnLoadHook = HookEvent(ME_SYSTEM_MODULESLOADED,extraInitMenus);
 	hOnExitHook = HookEvent(ME_SYSTEM_SHUTDOWN,realizeDBonExit);
-	hOkToExitHook = HookEvent(ME_SYSTEM_OKTOEXIT, OkToExitProc);
+	hOnPreShutdownHook = HookEvent(ME_SYSTEM_PRESHUTDOWN, AskToRealizeProc);
 
 #ifdef SECUREDB
 	CreateServiceFunction("DB3XS/MakeBackup",DB3XSMakeBackup);
