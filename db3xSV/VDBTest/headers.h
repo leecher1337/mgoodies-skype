8bfbe9dfde04c4cf293c2439c8cff00154636620
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2007-09-23 21:05:54 +0000

    made a copy

--- db3xSV/VDBTest/headers.h
+++
@@ -1,9 +0,0 @@
-#include <windows.h> 
-#include <newpluginapi.h>
-#include <m_options.h>
-#include <m_utils.h>
-#include <m_popup.h>
-#include <m_skin.h>
-#include "resource.h"
-
-int ShowPopup(const char *fmt, ...);
