fd3cdd297c4d5bd94d1e0adb84cf4790a4715e66
protogenes <protogenes@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2008-12-05 11:00:34 +0000

    dbx_tree: * enhanced BlockManager safety + release Thread handle of BlockManager OptimizeThread after it finished * fixed memory alloc/dealloc in IterationHeap + added GPL notice

--- trunk/dbx_tree/BlockManager.h
+++ trunk/dbx_tree/BlockManager.h
@@ -1,3 +1,25 @@
+/*
+
+dbx_tree: tree database driver for Miranda IM
+
+Copyright 2007-2008 Michael "Protogenes" Kunz,
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
 #pragma once
 
 #include <windows.h>
@@ -11,7 +33,7 @@
 #include "MREWSync.h"
 #include "Thread.h"
 
-class CBlockManager : private CThread
+class CBlockManager
 {
 protected:
 	static const uint32_t cFreeBlockID = 0xFFFFFFFF;
@@ -43,6 +65,16 @@ protected:
 
 	typedef std::multimap<uint32_t, uint32_t> TFreeBlockMap;
 
+	class COptimizeThread : public CThread
+	{
+	protected:
+		CBlockManager & m_Owner;
+		void Execute() {m_Owner.ExecuteOptimize();};
+	public:
+		COptimizeThread(CBlockManager & Owner) : CThread(true), m_Owner(Owner) {};
+		~COptimizeThread() {};
+	};
+
 	typedef struct TBlockTableEntry {
 		uint32_t Addr;
 	} TBlockTableEntry;
@@ -57,8 +89,8 @@ protected:
 	std::vector<uint32_t> m_FreeIDs;
 
 	uint32_t m_Optimize;
-
-	void Execute();
+	COptimizeThread * m_OptimizeThread;
+	void ExecuteOptimize();
 
 	void Read(uint32_t Addr, bool IsVirtual, void* Buffer, uint32_t Size);
 	void Write(uint32_t Addr, bool IsVirtual, void* Buffer, uint32_t Size);
--- trunk/dbx_tree/IterationHeap.h
+++ trunk/dbx_tree/IterationHeap.h
@@ -1,3 +1,25 @@
+/*
+
+dbx_tree: tree database driver for Miranda IM
+
+Copyright 2007-2008 Michael "Protogenes" Kunz,
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
 #pragma once 
 
 #include <vector>
@@ -52,7 +74,7 @@ CIterationHeap<TType>::CIterationHeap(TType & InitialItem, TIterationType MinMax
 	m_Heap.reserve(1 << 1);
 	m_Type = MinMax;
 	
-	m_Heap[0] = (THeapElement*)malloc(sizeof(THeapElement));
+	m_Heap[0] = new THeapElement;
 	m_Heap[0]->Elem = &InitialItem;
 	m_Heap[0]->Index = m_Heap.size();
 	m_DeleteItems = DeleteItems;
