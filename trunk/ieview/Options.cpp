274aacc22e1910cf25cc7dc2ec20beea84ec0ca9
the_leech <the_leech@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2010-10-09 12:47:56 +0000

    add VS 2010 project file, clean up includes, remove redundant files

--- trunk/ieview/Options.cpp
+++ trunk/ieview/Options.cpp
@@ -20,8 +20,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #define _WIN32_WINNT  0x0501
 #include "Options.h"
-#include <shlobj.h>
-#include <uxtheme.h>
 #include "resource.h"
 //#include "Smiley.h"
 #include "Template.h"
