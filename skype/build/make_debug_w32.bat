c5c2d0dd02135e666b5e6c39428c267fc23e8053
leecher@dose.0wnz.at <leecher@dose.0wnz.at@3cd5de0c-97d6-f89e-5382-93ac1fc1917b>
2014-08-14 10:12:01 +0000

    Restructured directory tree

---
+++ skype/build/make_debug_w32.bat
@@ -0,0 +1,5 @@
+call "F:\Programme\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT" 
+nmake -f Skype_protocol.mak CFG="Win32 UNICODE Debug" clean
+nmake -f Skype_protocol.mak CFG="Win32 UNICODE Debug"
+nmake -f Skype_protocol.mak CFG="Win32 Debug" clean
+nmake -f Skype_protocol.mak CFG="Win32 Debug"
